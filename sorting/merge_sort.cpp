fa7036469161732080b0bfa4fa2255983332c97b
peter-rich <yangzhanfu111@gmail.com>
2022-11-10 15:25:53 -0500

    changed sorting

--- sorting/merge_sort.cpp
+++ sorting/merge_sort.cpp
@@ -12,6 +12,7 @@ void merge(int arr[], int l, int m, int r)
 	//Merging two sorted arrays
 } 
 
+
 /* l is for left index and r is right index of the 
 sub-array of arr to be sorted */
 void merge_sort(int arr[], int l, int r) 
