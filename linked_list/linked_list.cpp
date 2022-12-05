e7df3f53908ed7ed78e84835c827dc35624debf7
peter-rich <zhanfu.yang@vesoft.com>
2022-12-05 15:08:34 -0500

    Update linked_list.cpp

--- linked_list/linked_list.cpp
+++ linked_list/linked_list.cpp
@@ -1,104 +1,43 @@
-#include <stdio.h>
-#include <cstdlib>
-#include <iostream>
-#include <assert.h>
-struct data {
-    int value;
-    struct data * next;
-};
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+#include<vector>
+#include<iostream>
+using namespace std;
 
-struct data * tail_linked_list(struct data *head_linked_list)
-{
-    struct data *ptr = head_linked_list;
-    while (ptr->next != NULL) ptr = ptr->next;
-//    printf("== Tail value = %d\n", ptr->value);
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+ 
 
-    return ptr;
+ListNode* merge2Lists(ListNode* list_a, ListNode* list_b) {
+    ListNode* res;
+    return res;
 }
 
-void insert_element_tail(struct data *head_linked_list, int val)
-{
-    struct data *tail = tail_linked_list(head_linked_list);
-    struct data *tmp = new struct data;
-    tail->next = tmp;
-    tmp->value = val;
-    tmp->next = NULL;
-}
-
-void insert_element(struct data *head_linked_list, int val_before_insertion, int val_to_insert)
-{
-    //Create a container to store the to-be-inserted value;
-    struct data *tmp = new struct data;
-    tmp -> value = val_to_insert;
-
-    //Search through the linked list to find "val_before_insertion" or 
-    //- if we didn't find it, it returns the tail
-    struct data *ptr = head_linked_list;
-    while ((ptr->value != val_before_insertion) && (ptr->next != NULL)) ptr = ptr->next;
-
-    //Let the created container store the next of "value_before_insertion"
-    tmp->next = ptr->next;
-
-    //Let the value_before_insertion's next store the created container
-    ptr->next = tmp;
-}
-
-
-void delete_element(struct data *head_linked_list, int val_to_delete)
-{
-    //Search through the linked list to find "val_to_delete" or 
-    //- if we didn't find it, it returns the tail
-    struct data *ptr_current = head_linked_list;
-    struct data *ptr_prior = NULL;
+int main() {
     
-    while ((ptr_current -> value != val_to_delete) && (ptr_current->next != NULL))
-    {
-        ptr_prior = ptr_current;
-        ptr_current = ptr_current->next;
-
-    }
-
-    //Either we find ptr_current->value == val_to_delete
-    if (ptr_current -> value == val_to_delete)
-        ptr_prior->next = ptr_current->next;
-}
-
-void print_linked_list(struct data *head_linked_list)
-{
-    struct data *ptr = head_linked_list;
-    while (ptr != NULL)
-    {
-        std::cout<<ptr->value<<" --> ";
-        ptr = ptr->next;
-    }
-    std::cout<<"NULL\n";
-}
-
-int main(int args, char **argv)
-{
-    struct data *my_linked_list = new struct data;
-    my_linked_list -> next = NULL;
-    my_linked_list -> value = 5;
-
-    //build the linked list;
-//    insert_element_tail(my_linked_list, 5);
-    std::cout<<"### Initial link list: ";
-    print_linked_list(my_linked_list);
-    insert_element_tail(my_linked_list, 2);
-    insert_element_tail(my_linked_list, 3);
-        
-    std::cout<<"### link list after insert element tail: ";
-    print_linked_list(my_linked_list);
+    ListNode *list_a = new ListNode(1);
+    list_a->next = new ListNode(4);
+    ListNode *tmp = list_a->next;
+    tmp->next = new ListNode(5);
     
-    insert_element(my_linked_list, 2, -1);
-
-    std::cout<<"### link list after insert: ";
-    print_linked_list(my_linked_list);
-
-    delete_element(my_linked_list, 2);
+    ListNode *list_b = new ListNode(1);
+    list_b->next = new ListNode(3);
+    tmp = list_b->next;
+    tmp->next = new ListNode(4);
     
-    std::cout<<"### link list after delete element: ";
-    print_linked_list(my_linked_list);
-
+    ListNode *list_c = merge2Lists(list_a, list_b);
     return 0;
 }
